1d03c9a21d9b13fb601ea8524f548eb57507e94f
christianjamestejo-ui <christianjames.tejo@dbtc-cebu.edu.ph>
2026-02-09 16:16:48 +0800

    Add files via upload
    Paired Activity for 2nd Semester

---
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,45 @@
+﻿// Tejo, Christian James F.; Remonde, Sean R.
+using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 3, num2 = 2;
+            Console.WriteLine($"{num1} + {num2} = {CalculateAdd( num1, num2)}");
+            string result1 = (num1 < num2) ? "Invalid Input" : $"{num1} - {num2} = {CalculateSub(num1, num2)}";
+            Console.WriteLine(result1);
+            Console.WriteLine($"{num1} x {num2} = {CalculateMulti(num1, num2)}");
+            if (num1 < num2)
+            {
+                Console.WriteLine("Invalid Input");
+            }
+            else if (num2 == 0)
+            {
+                Console.WriteLine("Invalid Input");
+            }
+            else
+            {
+                Console.WriteLine($"{num1} / {num2} = {CalculateDivi(num1, num2)}");
+            }
+        }
+        static int CalculateAdd(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        static int CalculateSub(int num1, int num2) 
+        {  
+            return num1 - num2; 
+        }
+        static int CalculateMulti(int num1, int num2)
+        { 
+            return num1 * num2;
+        }
+        static int CalculateDivi(int num1, int num2)
+        { 
+            return num1 / num2; 
+        }
+    }
+}
